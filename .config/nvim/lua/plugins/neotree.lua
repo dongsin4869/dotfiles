97cd7df1c0bcfb47b806a2842a6389b757794a18
Dongsin Kim <dongsin01@sogang.ac.kr>
2025-10-02 22:36:31 +0900

    neotree fix

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -28,6 +28,7 @@ return {
   },
   config = function()
     require("neo-tree").setup({
+      log_level = "info",
       close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
       popup_border_style = "rounded",
       enable_git_status = true,
