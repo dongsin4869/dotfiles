4b5b90e9fc8b64177d207f30d14804ee1083e3ff
Dongsin Kim <dongsin01@sogang.ac.kr>
2025-10-03 21:51:59 +0900

    noice.nvim

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -3,8 +3,27 @@ return {
   {
     "folke/noice.nvim",
     opts = function(_, opts)
+      opts.views = opts.views or {}
       opts.routes = opts.routes or {}
       opts.presets = opts.presets or {}
+
+      opts.views.cmdline_popup = {
+        position = {
+          row = 5,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      }
 
       table.insert(opts.routes, {
         filter = {
@@ -104,7 +123,7 @@ return {
       plugins = {
         gitsigns = true,
         tmux = true,
-        kitty = { enabled = false, font = "+2" },
+        wezterm = { enabled = false, font = "+4" },
       },
     },
     keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
