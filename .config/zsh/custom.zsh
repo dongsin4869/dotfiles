163e9830c8b2bad2957fb1d624fde9de3ab1ebf3
Dongsin Kim <dongsin01@sogang.ac.kr>
2025-10-19 18:21:50 +0900

    vi-mode

--- .config/zsh/custom.zsh
+++ .config/zsh/custom.zsh
@@ -136,5 +136,38 @@ function y() {
 eval $(thefuck --alias)
 eval $(thefuck --alias fk)
 
+# vi mode
+bindkey -v
+export KEYTIMEOUT=1
+
+autoload edit-command-line
+zle -N edit-command-line
+bindkey -M vicmd v edit-command-line
+
+export VI_MODE_SET_CURSOR=true
+
+function zle-keymap-select {
+  if [[ $KEYMAP == vicmd ]]; then
+    echo -ne '\e[2 q'  # block cursor
+  else
+    echo -ne '\e[6 q'  # beam cursor
+  fi
+}
+zle -N zle-keymap-select
+
+function zle-line-init() {
+  zle -K viins
+  echo -ne '\e[6 q'
+}
+zle -N zle-line-init
+
+# Yank to the system clipboard
+function vi-yank-clipboard {
+  zle vi-yank
+  echo $"CUTBUFFER" | pbcopy -i
+}
+zle -N vi-yank-clipboard
+bindkey -M vicmd 'y' vi-yank-clipboard
+
 # zoxide
 eval "$(zoxide init zsh)"
